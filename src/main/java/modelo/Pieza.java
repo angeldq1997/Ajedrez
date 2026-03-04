bfefe8f804f84a26b5117736b299b934327fc5a7
Daniel Ruiz <danielruizfernandez.07@gmail.com>
2026-03-04 14:45:19 +0100

    Feat: Realizado método validaPieza y modificado el constructor.

--- src/main/java/modelo/Pieza.java
+++ src/main/java/modelo/Pieza.java
@@ -4,24 +4,22 @@ public abstract class Pieza implements Serializable {
     private int columna;
     private int fila;
     private Color color;
-    private int puntos;
-    private TipoPieza tipoPieza;
-    private String forma;
+    private char icono;
 
-    public Pieza(int columna, int fila, Color color, int puntos, TipoPieza tipoPieza) {
-        this.columna = columna;
+    public Pieza(int fila, int columna, Color color) {
+        validaPosicion(fila, columna);
+        if (color == null){
+            throw new IllegalArgumentException ("Debes de introducir un color");
+        }
         this.fila = fila;
+        this.columna = columna;
         this.color = color;
-        this.puntos = puntos;
-        this.tipoPieza = tipoPieza;
     }
 
-    public String getForma() {
-        return forma;
-    }
-
-    public void setForma(String forma) {
-        this.forma = forma;
+    protected void validaPosicion (int fila, int columna){
+        if (fila < 0 || fila > 7 || columna < 0 || columna > 7){
+            throw new IllegalArgumentException("Debes de introducir un número entre 0-7");
+        }
     }
 
     public abstract boolean atacar();
